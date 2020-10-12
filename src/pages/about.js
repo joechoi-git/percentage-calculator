9bbc66b51977ad039b6c4e9e41cba4e2cc29f5e1
Joe Choi <Joe.Choi@rga.com>
2020-10-12 16:37:59 -0400

    added more stuff

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,34 @@
 import React from "react"
 import Header from "../components/header"
+import Layout from "../components/layout"
+import styles from "./about.module.css"
+
+const User = props => (
+    <div className={styles.user}>
+      <img src={props.avatar} className={styles.avatar} alt="" />
+      <div className={styles.description}>
+        <h2 className={styles.username}>{props.username}</h2>
+        <p className={styles.excerpt}>{props.excerpt}</p>
+      </div>
+    </div>
+)
 
 export default function About() {
   return (
-    <div style={{ color: `teal` }}>
+    <Layout>
       <Header headerText="About Gatsby" />
       <Header headerText="It's pretty cool" />
       <p>Such wow. Very React.</p>
-    </div>
+      <User
+        username="Jane Doe"
+        avatar="https://s3.amazonaws.com/uifaces/faces/twitter/adellecharles/128.jpg"
+        excerpt="I'm Jane Doe. Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+      />
+      <User
+        username="Bob Smith"
+        avatar="https://s3.amazonaws.com/uifaces/faces/twitter/vladarbatov/128.jpg"
+        excerpt="I'm Bob Smith, a vertically aligned type of guy. Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+      />
+    </Layout>
   )
 }
