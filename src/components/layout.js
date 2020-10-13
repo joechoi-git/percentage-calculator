f7d313469a1ffad9cedb368c5cd133e8c02e1341
Joe Choi <Joe.Choi@rga.com>
2020-10-13 12:00:13 -0400

    bootstrap

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,16 +1,34 @@
 import React from "react"
+import { Container, Row, Col, Visible } from "react-grid-system"; // https://github.com/sealninja/react-grid-system
 import styles from "./layout.module.css"
 
 export default function Layout({ children }) {
   return (
-    <div className={styles.layout}>
+    <Container className={styles.layout}>
       <header style={{ marginBottom: `1.5rem` }}>
         <h3>PercentCalculatorPro.com</h3>
+        <Visible xs sm>
+          <p> This is rendered only on mobile </p>
+        </Visible>
+        <Visible md lg xl xxl>
+          <p> This is rendered only on desktop </p>
+        </Visible>
+        <Row>
+          <Col sm={4}>
+            One of three columns
+          </Col>
+          <Col sm={4}>
+            One of three columns
+          </Col>
+          <Col sm={4}>
+            One of three columns
+          </Col>
+        </Row>
       </header>
       {children}
       <footer>
         <p>All rights reserved.</p>
       </footer>
-    </div>
+    </Container>
   )
 }
