84679139d1c4da050bfb796e031eecb5fc84e6f2
Joe Choi <Joe.Choi@rga.com>
2020-10-14 16:40:21 -0400

    social share

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,35 +1,67 @@
 import React from "react"
 import { Container, Row, Col, Visible } from "react-grid-system";
+import {
+  EmailShareButton,
+  EmailIcon,
+  FacebookShareButton,
+  FacebookIcon,
+  TwitterShareButton,
+  TwitterIcon,
+  LinkedinShareButton,
+  LinkedinIcon,
+  WhatsappShareButton,
+  WhatsappIcon,
+} from "react-share"; // https://github.com/nygardk/react-share/blob/master/demo/Demo.tsx
 import styles from "./layout.module.css"
 
 export default function Layout({ children }) {
+  const shareUrl = 'https://percentcalculatorpro.com/';
+  const title = 'Percent Calculator Pro';
   return (
     <Container className={styles.layout}>
       <header style={{ marginBottom: `1.5rem` }}>
-        <p className={styles.p}>PercentCalculatorPro.com</p>
-        {/*}
-        <Visible xs sm>
-          <p> This is rendered only on mobile </p>
-        </Visible>
-        <Visible md lg xl xxl>
-          <p> This is rendered only on desktop </p>
-        </Visible>
-        <Row>
-          <Col sm={4}>
-            One of three columns
-          </Col>
-          <Col sm={4}>
-            One of three columns
-          </Col>
-          <Col sm={4}>
-            One of three columns
-          </Col>
-        </Row>
-      {*/}
+        <p className={styles.logo}>PercentCalculatorPro.com</p>
       </header>
       {children}
       <footer style={{ marginTop: `1.5rem` }}>
-        <p>&copy; 2020 Joe Choi</p>
+        <Row>
+          <Col sm={4}>
+            <p>&copy; 2020 Joe Choi</p>
+          </Col>
+          <Col sm={8} className={styles.socialshare}>
+            <EmailShareButton
+              url={shareUrl}
+              subject={title}
+              body="Check out PercentCalculatorPro.com"
+            >
+              <EmailIcon size={32} round />
+            </EmailShareButton>
+            <FacebookShareButton
+              url={shareUrl}
+              quote={title}
+            >
+              <FacebookIcon size={32} round />
+            </FacebookShareButton>
+            <TwitterShareButton
+              url={shareUrl}
+              title={title}
+            >
+              <TwitterIcon size={32} round />
+            </TwitterShareButton>
+            <LinkedinShareButton 
+              url={shareUrl}
+            >
+              <LinkedinIcon size={32} round />
+            </LinkedinShareButton>
+            <WhatsappShareButton
+              url={shareUrl}
+              title={title}
+              separator=":: "
+            >
+              <WhatsappIcon size={32} round />
+            </WhatsappShareButton>
+          </Col>
+        </Row>
       </footer>
     </Container>
   )
